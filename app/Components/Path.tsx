909e8a9d0b94a3bd7eee67975c07188f13e6ff09
Aguinaldo <Aguinaldofreire.ds@gmail.com>
2024-06-21 10:27:55 -0300

    1

--- app/Components/Path.tsx
+++ app/Components/Path.tsx
@@ -5,8 +5,12 @@ import styles from './Path.module.css';
 const Path = () => {
   return (
     <svg width="400" height="200" viewBox="0 0 400 200" className={styles.svg}>
-      <path 
-        d="M 10 100 H 150 A 50 50 0 1 1 250 100 H 390" 
+      <path   
+        d="M 10 50 
+           H 50 
+           A 5 5 0 1 0 60 50
+           A 5 5 0 1 0 50 50 
+           H 390" 
         className={styles.path}
       />
     </svg>
