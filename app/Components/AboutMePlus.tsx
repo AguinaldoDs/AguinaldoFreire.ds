2423debc95698ac35a907a4a4276d5ed30c99ae4
Aguinaldo <Aguinaldofreire.ds@gmail.com>
2024-06-23 01:45:44 -0300

    1

---
+++ app/Components/AboutMePlus.tsx
@@ -0,0 +1,17 @@
+{/* About me */}
+
+import { motion } from "framer-motion";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+
+export default function AboutMe(){
+return(
+<main className="flex">
+<div>
+  Testando
+</div>
+
+</main>)
+};
--- app/Routes/Home/page.tsx
+++ app/Routes/Home/page.tsx
@@ -4,10 +4,16 @@ import { motion } from "framer-motion";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import AboutMe from "@/app/Components/AboutMePlus";
 
 const linkLinkedin = () => {
   window.open('https://www.linkedin.com/in/aguinaldo-freire-95bb5a181/', '_blank');
 };
+
+// Parametros About me
+
+//
 
 const arrayPontinhos = [".", ".", "."].map((value, indice) => {
   return (
@@ -48,6 +54,11 @@ export default function Home() {
       behavior: 'smooth'
     });
   };
+
+  const { ref: ref1, inView: inView1 } = useInView({ triggerOnce: true });
+  const { ref: ref2, inView: inView2 } = useInView({ triggerOnce: true });
+  const { ref: ref3, inView: inView3 } = useInView({ triggerOnce: true });
+  const { ref: ref4, inView: inView4 } = useInView({ triggerOnce: true });
 
   return (
     <main className=" bg-gray-950 h-[2000px] w-full">
@@ -101,75 +112,103 @@ export default function Home() {
       </div>
 
 {/* Profile */}
-
       <div className="font-extralight flex mt-[18%] w-full h-auto justify-center items-center"> 
-      <motion.div
+        <motion.div
           className="flex bg-red-400 w-80 h-80 justify-center items-center rounded-3xl"
-          >
-            
-      </motion.div>
-        <div  className="font-extralight flex relative flex-col ml-10 ">
-        <p>Analista de Dados e MIS com dois anos de experiência em manipulação, análise e visualização de dados.</p>
-        <p>Especialista em Python, MSSQL, Business Intelligence (BI) e R.</p>
-        <p>Além de desenvolvedor Web, dominando React Next, Node.js e Tailwind.</p>
+        >
+        </motion.div>
+        <div className="font-extralight flex relative flex-col ml-10 ">
+          <p>Analista de Dados e MIS com dois anos de experiência em manipulação, análise e visualização de dados.</p>
+          <p>Especialista em Python, MSSQL, Business Intelligence (BI) e R.</p>
+          <p>Além de desenvolvedor Web, dominando React Next, Node.js e Tailwind.</p>
         </div>
       </div>
 
-
 {/* About me */}
-
       <div className="flex relative mt-[10%] h-1 items-center">
-        <motion.div
-        className="flex absolute bg-white w-[70%] h-[1px] rounded-3xl left-2">
+        <motion.div className="flex absolute bg-white w-[70%] h-[1px] rounded-3xl left-2">
         </motion.div>
-        <span className="flex relative ml-[75%] font-extralight text-5xl border-solid 
-        "> About me </span>
+        <span className="flex relative ml-[75%] font-extralight text-5xl border-solid">
+          About me
+        </span> 
       </div> 
+      <div className="flex w-max font-medium text-[12px] space-y-2 mt-10 justify-center ml-[50%] -translate-x-[50%]">
+        <div className="w-max space-y-1">
+          <motion.div
+            ref={ref1}
+            className="flex gap-7"
+            initial={{opacity: 0, x: -50}}
+            animate={inView1 ? {opacity: 1, x: 0} : {opacity: 0, x: -50}}
+            transition={{duration: 1}}
+          >
+            <Image src="/images/sql.svg" alt="Exemplo" width={60} height={60} />
+            <div>
+              <p>Em meados 2022 inicie minha carreira como Analista de dados em</p>
+              <p>uma empresa no ramo de telecobrança, onde desenvolvi grandes</p>
+              <p>habilidades com MSSQL, nela a crianção de procedures, Triggers,</p>
+              <p>Views e trabalhados agendados.</p>
+            </div>
+          </motion.div>
+          <p>&nbsp;</p>
 
-{/* About me */}
+          <motion.div
+            ref={ref2}
+            className="flex gap-7"
+            initial={{opacity: 0, x: -50}}
+            animate={inView2 ? {opacity: 1, x: 0} : {opacity: 0, x: -50}}
+            transition={{duration: 1}}
+          >
+            <Image src="/images/bi.svg" alt="Exemplo" width={60} height={60} />
+            <div>
+              <p>Além de Power BI para motagem de DashBoards e transformar de fatos</p>
+              <p> informações cruas dos bancos de dados em informações visuais que </p>
+              <p>auxiliam na tomada de dicisões.</p>
+            </div>
+          </motion.div>
+          <p>&nbsp;</p>
 
-  
-          <div className="flex w-max font-medium text-[12px] space-y-2 mt-10
-           justify-cente  ml-[50%] -translate-x-[50%]">
-          <div className=" w-max space-y-1
-           ">
-            
-          <p>Em meados 2022 inicie minha carreira como Analista de dados em</p>
-          <p>uma empresa no ramo de telecobrança, onde desenvolvi grandes</p>
-          <p>habilidades com MSSQL, nela a crianção de procedures, Triggers,</p>
-          <p>Views e trabalhados agendados.</p>
+          <motion.div
+            ref={ref3}
+            className="flex gap-7"
+            initial={{opacity: 0, x: 50}}
+            animate={inView3 ? {opacity: 1, x: 0} : {opacity: 0, x: 50}}
+            transition={{duration: 1}}
+          >
+            <Image src="/images/py2.svg" alt="Exemplo" width={60} height={60} />
+            <div>
+              <p>Outra grande ferramenta que criei dominio foi a linguagem Python,</p>
+              <p>permitindo agilizar importações de informações, também me auxiliou</p>
+              <p>na automação de um processo manual que era feito na empresa que</p>
+              <p>custava minutos importantes do meu dia. </p>
+            </div>
+          </motion.div>
           <p>&nbsp;</p>
-          <p>Além de Power BI para motagem de DashBoards e transformar de fatos </p>
-          <p> informações cruas dos bancos de dados em informações visuais que  </p>
-          <p>auxiliam na tomada de dicisões.</p>
-          <p>&nbsp;</p>
-          <p>Outra grande ferramenta que criei dominio foi a linguagem Python,</p>
-          <p>permitindo agilizar importações de informações, também me auxiliou</p>
-          <p>na automação de um processo manual que era feito na empresa que</p>
-          <p>custava minutos importantes do meu dia. </p>
-          <p>&nbsp;</p>
-          <p>Meu maior desafio nessa empresa, foi a implantação de um novo cliente</p>
-          <p>do completo zero, fiz a modelagem das ETLs, criação das rotinas e as</p>
-          <p>visões para acompanhamento dos times operacionais. Recebi grandes</p>
-          <p>responsabilidades e consegui fazer todas as entregas usando o método</p>
-          <p>Ágil de prioridades.</p>
-          
-          
+
+          <motion.div
+            ref={ref4}
+            className="flex gap-7"
+            initial={{opacity: 0, x: 50}}
+            animate={inView4 ? {opacity: 1, x: 0} : {opacity: 0, x: 50}}
+            transition={{duration: 1}}
+          >
+            <Image src="/images/trf.svg" alt="Exemplo" width={60} height={60} />
+            <div>
+              <p>Meu maior desafio nessa empresa, foi a implantação de um novo cliente</p>
+              <p>do completo zero, fiz a modelagem das ETLs, criação das rotinas e as</p>
+              <p>visões para acompanhamento dos times operacionais. Recebi grandes</p>
+              <p>responsabilidades e consegui fazer todas as entregas usando o método</p>
+              <p>Ágil de prioridades.</p>
+            </div>
+          </motion.div>
+
           <div className="cursor-pointer w-[50%] ml-[50%] -translate-x-[50%] bg-red-400 rounded-md 
-                          p-2 justify-cente text-center">
-           About me+ </div>
-           </div>
-
-      <Image src="/images/sql.svg" alt="Exemplo" width={60} height={60} /> 
-
-
+            p-2 justify-center text-center hover:bg-red-500 duration-300">
+            About me+
           </div>
-         
+          <AboutMe/>
+        </div>
 
-
-
-
-
+      </div>
     </main>
   );
 }
