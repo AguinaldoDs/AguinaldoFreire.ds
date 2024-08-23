383e2ae5367f088d41d562a6fdb2d12082d66cd5
Aguinaldo <Aguinaldofreire.ds@gmail.com>
2024-08-23 15:36:55 -0300

    1

--- app/Routes/Test/page.tsx
+++ app/Routes/Test/page.tsx
@@ -1,21 +1,12 @@
 // pages/index.tsx
 'use client'
-import React, { useState } from 'react';
+import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <main>
-    <title> Import </title>
-    <div className="flex items-center justify-center min-h-screen bg-gray-950">
-      <form className="bg-white p-6 rounded-lg shadow-lg">
-        <label htmlFor="arquivo" className="block text-gray-700 font-bold mb-2">Escolha um arquivo:</label>
-        <input type="file" id="arquivo" name="arquivo"  className="block w-full text-gray-900 py-2 px-3 border border-gray-300 rounded-md mb-4" />
-        
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Enviar
-        </button>
-      </form>
-    </div>
+    <main className='flex flex-col bg-slate-100 w-full overflow-y-auto h-dvh'>
+      <title>I'm seeing you! 👀</title>
+
     </main>
   );
 };
